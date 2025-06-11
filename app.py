22b60156b547e8e5248248b404be981addfabfa9
Wycofane <julianwyco@gmail.com>
2025-06-11 13:22:59 +0200

    added gevent wsgi server

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from gevent import pywsgi
 
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 if __name__ == '__main__':
+    http_server = pywsgi.WSGIServer(('0.0.0.0', 80), app)
+    http_server.serve_forever()
 
-    app.run(host="0.0.0.0", port=80)
+    # app.run(host="0.0.0.0", port=80)
 
